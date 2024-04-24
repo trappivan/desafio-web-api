c4b5b6c5fff377840d298800c2bc4d905e82f57a
ivan <ivan.trapp@univille.br>
2024-04-24 12:27:30 -0300

    docker fixing

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,34 +6,37 @@ import cors, { CorsOptions } from "cors";
 import cookieParser from "cookie-parser";
 
 config();
+try {
+	AppDataSource.initialize();
+} catch (error) {
+	console.log("errorr", error);
+	throw new Error("Database connection error");
+}
+console.log("Database connected");
 
 const main = async () => {
 	try {
-		await AppDataSource.initialize();
+		const app = express();
 
-		console.log("Database connected");
+		app.use(express.json());
+
+		let corsOptions: CorsOptions = {
+			origin: "http://localhost:3000",
+			credentials: true,
+			exposedHeaders: ["set-cookie"],
+		};
+		app.use(cors(corsOptions));
+
+		app.use(cookieParser(process.env.TOKEN_SECRET!));
+
+		app.use(routes);
+
+		app.listen(process.env.PORT, () => {
+			console.log(`Server running on port ${process.env.PORT}`);
+		});
 	} catch (error) {
-		console.error(error);
+		throw new Error("Database connection error");
 	}
-
-	const app = express();
-
-	app.use(express.json());
-
-	let corsOptions: CorsOptions = {
-		origin: "http://localhost:3000",
-		credentials: true,
-		exposedHeaders: ["set-cookie"],
-	};
-	app.use(cors(corsOptions));
-
-	app.use(cookieParser(process.env.TOKEN_SECRET!));
-
-	app.use(routes);
-
-	app.listen(process.env.PORT, () => {
-		console.log(`Server running on port ${process.env.PORT}`);
-	});
 };
 
 main();
